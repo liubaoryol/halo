502d89d035825e97ad2c6915f87f17a785927c94
Liuba Orlova <liuba1156@gmail.com>
2025-05-22 22:40:10 -0500

    Experiments

--- experiments/rescueworldn3.sh
+++ experiments/rescueworldn3.sh
@@ -3,3 +3,14 @@ python3 train_hbc.py --config-name=train_rw_n3 student_type=latent_entropy_based
 python3 train_hbc.py --config-name=train_rw_n3 student_type=latent_entropy_based seed=2 query_percentage_budget=0.1
 python3 train_hbc.py --config-name=train_rw_n3 student_type=latent_entropy_based seed=3 query_percentage_budget=0.1
 python3 train_hbc.py --config-name=train_rw_n3 student_type=latent_entropy_based seed=4 query_percentage_budget=0.1
+python3 train_hbc.py --config-name=train_rw_n2 student_type=supervised seed=0
+python3 train_hbc.py --config-name=train_rw_n2 student_type=supervised seed=1
+python3 train_hbc.py --config-name=train_rw_n2 student_type=supervised seed=2
+python3 train_hbc.py --config-name=train_rw_n2 student_type=supervised seed=3
+python3 train_hbc.py --config-name=train_rw_n2 student_type=supervised seed=4
+python3 train_hbc.py --config-name=train_rw_n2 student_type=unsupervised seed=0
+python3 train_hbc.py --config-name=train_rw_n2 student_type=unsupervised seed=1
+python3 train_hbc.py --config-name=train_rw_n2 student_type=unsupervised seed=2
+python3 train_hbc.py --config-name=train_rw_n2 student_type=unsupervised seed=3
+python3 train_hbc.py --config-name=train_rw_n2 student_type=unsupervised seed=4
+
